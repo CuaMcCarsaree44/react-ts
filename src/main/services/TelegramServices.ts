c5a4e37a408c2073362b006a9b1e55c77bc26f00
yosuakristianto144_ <yosua.kristianto@indosystem.com>
2022-08-03 15:35:34 +0700

    Added TelegramServices

--- src/main/services/TelegramServices.ts
+++ src/main/services/TelegramServices.ts
@@ -1,6 +1,8 @@
 import {TelegramKeyNotPresetException} from "../common/exception/glo/TelegramKeyNotPresetException";
 import axios from 'axios';
 import {Log} from "../config/Logging";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export class TelegramServices {
 
@@ -12,11 +14,11 @@ export class TelegramServices {
    * So I'm gonna limit the characters to 2000 characters for now.
    */
   static TELEGRAM_CHAT_CHARACTERS_LIMIT: number = 2000;
-  private telegramBaseUrl = "";
+  private telegramBaseUrl = "https://api.telegram.org/bot";
 
   private uri = (): string => {
 
-    const key: string | undefined = process.env.TELEGRAM_URI;
+    const key: string | undefined = process.env.REACT_APP_TELEGRAM_API_KEY;
 
     if(key === undefined){
       throw new TelegramKeyNotPresetException();
@@ -27,8 +29,6 @@ export class TelegramServices {
 
   public async sendMessage(message: string, log?: boolean): Promise<void> {
 
-    if(!['local'].includes(process.env.APP_ENV!)){ return; }
-
     if(message.length > TelegramServices.TELEGRAM_CHAT_CHARACTERS_LIMIT){
       message =
         `${message.substring(0, TelegramServices.TELEGRAM_CHAT_CHARACTERS_LIMIT - 3)} `
@@ -37,13 +37,13 @@ export class TelegramServices {
 
     const uri: string = `${this.uri()}/sendMessage`;
     const request: any = {
-      chat_id: process.env.TELEGRAM_CHAT_ID,
-      text: `[${process.env.APP_ENV}]\n\n` + message
+      chat_id: process.env.REACT_APP_TELEGRAM_CHAT_ID,
+      text: `[${process.env.REACT_APP_ENV}]\n\n` + message
     };
 
     axios.post(uri, request)
       .catch(error => {
-        Log.e("Telegram - Send Message", error.message, error, false);
+        Log.e("Telegram - Send Message", error.message, error);
       });
   }
 }
