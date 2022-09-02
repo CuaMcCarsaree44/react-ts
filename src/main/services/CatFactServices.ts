a5e48168e74c34cf7a5d833af2763a3cf6d5dce3
yosuakristianto144_ <yosua.kristianto@indosystem.com>
2022-09-02 18:56:45 +0700

    Removed all not needed files

--- src/main/services/CatFactServices.ts
+++ src/main/services/CatFactServices.ts
@@ -1,7 +1,5 @@
 import {CatGetFactResponse} from "../model/response/CatGetFactResponse";
 import axios from "axios";
-import {Log} from "../config/Logging";
-
 
 export class CatFactServices {
 
@@ -19,8 +17,6 @@ export class CatFactServices {
         "length": e.data.length
       }
     }).catch((error: any) => {
-      Log.e("Cat Fact Services - Get New Fact", error.message, error);
-
       return {
         "fact": "",
         "length": 0
--- src/res/view/Home.tsx
+++ src/res/view/Home.tsx
@@ -1,8 +1,8 @@
 import React, {ReactNode} from "react";
 import logo from './../assets/logo.svg';
 import './../stylesheets/App.css';
+import './../stylesheets/dist/main.css'
 import dotenv from 'dotenv';
-import {HomeHandler} from "../../main/handler/HomeHandler";
 import {CatFactServices} from "../../main/services/CatFactServices";
 dotenv.config();
 
@@ -31,35 +31,9 @@ export class Home extends React.Component<Props, State> {
     })
   }
 
-
-  private homeHandler = new HomeHandler();
-
-  // Catch kinthil button click event
-  private handleKinthilButtonClick = async () => {
-    await this.homeHandler.testSendMessage();
-  }
-
-  private handleInputTextboxChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      "input_textbox": e.target.value
-    });
-  }
-
-  private handleCustomMessageSend = async () => {
-    await this.homeHandler.sendMessageWithString(this.state.input_textbox);
-  }
-
   public render = (): ReactNode => {
     return (
       <div className="App">
-
-        <button onClick={this.handleKinthilButtonClick}>Kinthil</button>
-
-        <div>
-          <input type="text" id="input-textbox" onChange={this.handleInputTextboxChange} />
-          <button onClick={this.handleCustomMessageSend}>Send Message</button>
-        </div>
-
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
